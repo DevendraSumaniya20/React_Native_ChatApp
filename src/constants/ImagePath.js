738f445d8659fc18cb56e94e91e52971c03df4ef
Sumaniya Devendra <dsumniya@gmail.com>
2023-09-28 20:40:23 +0530

    add new files

--- src/constants/ImagePath.js
+++ src/constants/ImagePath.js
@@ -1,1 +1,5 @@
-export default {};
+export default {
+  PROFILE: require('../assets/Images/user.png'),
+  GROUP: require('../assets/Images/group.png'),
+  SETTING: require('../assets/Images/setting.png'),
+};
